543dbd4b47a297540dc4839ced3dcadf186af062
jihye <thoth_0302@naver.com>
2024-02-27 12:47:52 +0900

    <feat> QuestList에 있는 제목을 받아와서 그에 맞춰 다른 text 수정

--- Assets/Scripts/Home/Quest.cs
+++ Assets/Scripts/Home/Quest.cs
@@ -1,18 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Quest : PopupUIBase
 {
-    // Start is called before the first frame update
-    void Start()
+    //QuestList에 잇는 title내용을 가져와야함.
+    //연결부분은 QuestListpopupui.
+    public TMP_Text QuestNumber;
+    public TMP_Text QuestTitle;
+    public TMP_Text QuestDescription;
+    public TMP_Text QuestGold;
+    public TMP_Text QuestClear;
+
+    QuestList questList;
+
+    public string[] questName = { "슬라임 처치", "총기 구매", "휴식하기", "총기 변경" };
+    public string[] questDescription = { 
+        "슬라임을 한마리 처치", 
+        "상점에서 총 구매하기", 
+        "회복 버튼을 눌러서 휴식하기", 
+        "구매한 총으로 변경하기" };
+    public int[] questGold = { 2000, 1500, 500, 1000 };
+
+    private void Start()
     {
-        
+        SettingQuest();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GetQuestList(QuestList list)
     {
-        
+        questList = list;
+    }
+
+    public void ChangeQuestClearText()
+    {
+        questList.QuestClearCheck(true);
+        QuestClear.text = questList.questListClear.text;
+    }
+
+
+
+    //처음 세팅
+    public void SettingQuest()
+    {
+        QuestNumber.text = questList.questListNumber.text; //안변할 것
+        QuestTitle.text = questList.questListTitle.text;//안변할 것
+        QuestClear.text = questList.questListClear.text;//버튼 누르면 미수락 -> 수락, 수락상태에서 퀘스트 클리어할 때까지 버튼 못누름
+
+        QuestDescriptionSetting(QuestTitle.text); //안변할 것
+    }
+
+    public void QuestDescriptionSetting(string name)
+    {
+        for(int i = 0; i < questName.Length; i++)
+        {
+            if (questName[i] == name)
+            {
+                QuestDescription.text = questDescription[i];
+                QuestGold.text = questGold[i].ToString();
+                break;
+            }
+        }
     }
 }
