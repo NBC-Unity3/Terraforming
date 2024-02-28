c77169cee9f39050cdbcacf5c06e82c0608d903a
박지훈 <150890216+art-riel@users.noreply.github.com>
2024-02-28 14:06:18 +0900

    feat
    원거리 몬스터 추가

---
+++ Assets/Scripts/Monster/Mucus.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mucus : MonoBehaviour
+{
+    public LayerMask target;
+    public int damage;
+    private Rigidbody _rigidbody;
+    private void Start()
+    {
+        Destroy(gameObject,3f);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        // 이거 안됨
+        //if(target.value == (target.value|1<<collision.gameObject.layer))
+        //{
+        //    //플레이어 체력 감소
+
+        //    Destroy(gameObject);
+        //}
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            //플레이어 체력 감소
+
+            Destroy(gameObject);
+        }
+    }
+
+}
